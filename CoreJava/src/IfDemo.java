f2d7b978836cc3c796a68d261b129a0fbd35bf7c
Tanishka-24May-SE <tanishkasoni510@gmail.com>
2023-03-16 10:12:48 +0530

    Second

---
+++ CoreJava/src/IfDemo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class IfDemo {
+          public static void main(String[] args) {
+			int a,b;
+			System.out.println("Enter a number:");
+			Scanner
+			if(a<b)
+				System.out.println("a is lagest");
+			else
+				System.out.println("b is lagest");
+		}
+}
